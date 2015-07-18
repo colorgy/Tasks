9d294130d74205ef2e34b608c1208618f2ddf64b
ChengHsuanLiu <chenghsuango@gmail.com>
2015-07-18 20:49:47 +0800

    colorgy login setting

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :trackable, :omniauthable,
+         :omniauth_providers => [:colorgy]
+
+  def self.from_colorgy(auth)
+    user = where(:id => auth.info.id).first_or_create! do |new_user|
+      new_user.email = auth[:info][:email]
+    end
+
+    attrs = %i(avatar_url name fbid fbtoken)
+
+    oauth_params = ActionController::Parameters.new(auth.info)
+    user_data = oauth_params.slice(*attrs).permit(*attrs)
+
+    user_data['refreshed_at'] = Time.now
+    # user_data['core_access_token'] = auth.credentials.token
+
+    user.update!(user_data)
+
+    return user
+  end
+end
